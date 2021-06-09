dba8d02c09bec28ec2cbfb58104cbe2def417183
hanseul777 <hanseul6343@daum.net>
2021-06-09 15:10:45 +0900

    .

--- hello2.js
+++ hello2.js
@@ -2,5 +2,5 @@ console.log("오호라..나는야 A개발자");
 console.log("Hello, world!");  
 console.log("나는야 B 개발자");
 var studentName = "홍길동";
-console.log;pageXOffset("이름:" + studentName);
+console.log("이름:" + studentName);
 
