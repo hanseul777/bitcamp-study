7b3deb133054ef0f69c931572d923aa151a7f53a
hanseul777 <hanseul6343@daum.net>
2021-07-07 09:55:50 +0900

    .

--- Test/hanseul.sql
+++ Test/hanseul.sql
@@ -1,7 +1,23 @@
+drop table hanseul;
+
 create table hanseul (
- id number(4) not null primary key
+id number(4) not null primary key,
+email varchar2(10) not null,
+phone number(12) not null
 );
 
-insert into hanseul values(22);
+insert into hanseul(id, email, phone) values (111, 'a@han', 010);
+insert into hanseul(id, email, phone) values (222, 'b@han', 3305);
+insert into hanseul(id, email, phone) values (333, 'c@han', 6343);
+
+select * from hanseul;
+
+alter table hanseul add ( name varchar2(13) );
+alter table hanseul drop column name;
+
+alter table hanseul add ( name number(10) );
+alter table hanseul modify ( name varchar2(10) );
+
+alter table hanseul rename column name to username;
 
 commit;
