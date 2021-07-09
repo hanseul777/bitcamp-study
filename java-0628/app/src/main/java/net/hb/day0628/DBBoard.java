d8699435df7170c1dae86d23949de52fb67bdb82
hanseul777 <hanseul6343@daum.net>
2021-07-09 10:16:53 +0900

    .

---
+++ java-0628/app/src/main/java/net/hb/day0628/DBBoard.java
@@ -0,0 +1,84 @@
+package net.hb.day0628;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DBBoard { //test테이블
+  Connection CN=null; //DB서버연결정보 서버ip주소 계정id,pwd
+  Statement ST=null;  //ST=CN.createStatement()명령어생성 삭제,신규등록,조회하라
+  ResultSet RS=null;  //select조회결과값 전체데이터를 기억합니다
+  String msg="";
+  int Gtotal = 0;  
+  Scanner sc = new Scanner(System.in);
+  //CN,ST,RS,msg,Gtotal,sc 전역변수는 main에서 사용불가 
+
+  public void dbConnect() {
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클드라이브로드
+      CN=DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","system","1234");
+      System.out.println("07-09-금요일 오라클 드라이브및 서버연결성공 ");     
+      ST = CN.createStatement(); //반드시필수 
+    }catch(Exception ex){System.out.println("error =" + ex);}
+  }//end
+
+  public void dbInsert() {
+    try {
+
+    }catch(Exception ex) { }   
+  }//end
+
+  public void dbSelectAll() {
+    try {
+      ST = CN.createStatement();
+      msg ="select  * from  test  order by code "; //문자열을 명령어처리 
+      ResultSet rs = ST.executeQuery(msg);
+      while(rs.next()==true) {
+        int a = rs.getInt("code");
+        String b = rs.getString("name");
+        String c = rs.getString("title");
+        System.out.println(a+"\t"+b+"\t" +c);
+      }
+      rs.close();
+    }catch(Exception ex){System.out.println("전체조회실패 =" + ex); }    
+  }//end
+
+  public void dbDelete() {
+    try {
+      //삭제처리는 삭제대상 필드를 정하기
+      //삭제처리는 99% where 조건절
+      System.out.print("삭제이름 입력>>> ");
+      String del = sc.nextLine(); //sc.next() : 공백주면 분리  sc.nextLine() : 엔터까지 입력 
+      msg = "delete from test where name = '" + del +"' ";
+      System.out.println(msg);
+      ST = CN.createStatement();
+      int OK = ST.executeUpdate(msg);
+      if (OK>0) {
+        System.out.println(del + "데이터 삭제성공");
+        dbSelectAll();
+      }else {
+        System.out.println(del + "데이터 삭제실패");
+      }
+
+      //msg문자열을 sql명령어 인식 : Statement
+      String msg1 = "insert into ~~ ";
+      String msg2 = "delete where ~~ ";
+      String msg3 = "update T명 set where ~~ ";
+      String msg4 = "select ~~ "; 
+      //       msg1, msg2, msg3 처리는 ST.executeUdate(쿼리문장)
+      //msg4 처리는 ST.executeQuery(쿼리문장)
+    }catch(Exception ex) { }      
+  }//end
+
+  public static void main(String[] args) {//메인메소드는 메모리에 자동로드=실행
+    try { 
+      DBBoard bbs = new DBBoard();
+      bbs.dbConnect(); //생략하면 문법에러 없습니다
+      bbs.dbSelectAll();
+      bbs.dbDelete();
+
+    }catch(Exception ex){ }
+  }//end
+}//Board class END
--- java-0628/app/src/main/java/net/hb/day0628/DBTest.java
+++ java-0628/app/src/main/java/net/hb/day0628/DBTest.java
@@ -1,16 +1,105 @@
 package net.hb.day0628;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class DBTest {
 
-  public static void main(String[] args) {
-    Connection CN = null; //DB서버연결정보
-    Statement ST = null; //명령어 생성
-    ResultSet RS = null; //조회결과를 기억
+  Connection CN = null; //DB서버연결정보 : ip주소, 계정id와 pw 등 기억
+  Statement ST = null; //명령어 생성 : 삭제, 신규등록, 조회 등 명령내림
+  ResultSet RS = null; //Select조회결과값 전체데이터를 
+  String msg = "isud = crud 쿼리기술문";    
+  Scanner sc = new Scanner(System.in);
 
+  public static void main(String[] args) {
+    DBTest DB = new DBTest();
+
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클드라이브로드
+      String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+      DB.CN = DriverManager.getConnection(url, "system", "1234");
+
+      //첫 번째 명령어 생성
+      DB.ST = DB.CN.createStatement();
+
+      loop: while (true) {
+        System.out.println("메뉴선택");
+        System.out.println("1. 코드입력");
+        System.out.println("2. 코드출력");
+        System.out.println("3. 코드삭제");
+        System.out.print("메뉴를 선택 (1..3) (0입력시 종료): ");
+        String s = DB.sc.nextLine();
+        switch (s) {
+          case "1" : DB.input(); break;
+          case "2" : DB.output(); break;
+          case "3" : DB.dbDelete(); break;
+          case "0" : break loop;
+        }
+
+      }
+
+    }catch (Exception ex) {System.out.println("에러이유" + ex);}
+    DB.sc.close();
+
+  }//main close
+
+  public void input() {
+    try {
+      //키보드에서 데이터 입력
+      System.out.print("\n코드입력(4자릿수) : ");
+      int a = Integer.parseInt(sc.nextLine());
+      System.out.print("이름입력 : ");
+      String b = sc.nextLine();
+      System.out.print("제목입력 : ");
+      String c = sc.nextLine();
+
+      //3번째 쿼리문 완성
+      //      msg = "insert into test (code,name,wdate,cnt) values(a,'b', 'c', sysdate, 0)";
+      msg="insert into test(code,name,title,wdate,cnt) values("+a+", '"+b+"', '"+c+"', sysdate, 0)";
+      System.out.println(msg);
+
+      //4번째 서버에서 실행 executeUpdate("insert ~~")
+      int OK = ST.executeUpdate(msg);
+      if (OK>0) {
+        System.out.println(a + "코드 데이터 저장성공했습니다.");
+      } else {System.out.println(a + "코드 데이터 저장실패했습니다.");}
+
+    }catch (Exception e) {System.out.println("에러이유 : "+ e);}
   }
 
-}
+  public void output() {
+    try {
+      System.out.println("프로그램 전체데이터 읽어오는 중... 잠시 기다려 주세요.");
+      Thread.sleep(3000);
+      msg = "select * from test" ; //문자열을 명령어 인식해서 실행하도록 Statement
+      RS = ST.executeQuery(msg);
+
+      System.out.println("코드\t이름\t");
+      while(RS.next()==true) {
+        //필드접근해서 데이터가져올 때 getXXX()
+        int ucode = RS.getInt("code");
+        String uname = RS.getString("name");
+        System.out.println((ucode + "\t" + uname));
+      }
+    }catch (Exception e) {System.out.println("에러이유 : "+ e);}
+  }
+
+  public void dbDelete() {
+    try {
+      System.out.print("삭제할 이름 입력 : ");
+      String del = sc.nextLine();
+      msg = "delete from test where name = " + del + "'";
+      System.out.println(msg);
+      int OK = ST.executeUpdate(msg);
+      if (OK>0){
+        System.out.println(del+"데이터 삭제 성공");   
+      }else {System.out.println(del+"데이터 삭제 실패");}
+
+    }catch (Exception e) {System.out.println("에러이유 : "+ e);}
+  }
+
+
+}//class close
